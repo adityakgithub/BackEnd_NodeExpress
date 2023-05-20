169506fd3eaf20ecc02920c4befe5f884016af80
adityakgithub <adityakorade@gmail.com>
2023-05-20 17:22:37 +0530

    sql module of node added.

---
+++ module/mySQL/sql.js
@@ -0,0 +1,24 @@
+const mySql = require("mysql");
+
+function getProduct(){
+    //1.Create Connection 
+    const connection = mySql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "ecommerce_db"
+    })
+    //2.Create Database Query
+    const statement =`select * from table_Name`
+    //3.Execute the Query Created by Database
+    connection.query(statement, (err, result) => {
+        if(err) throw err;
+        console.log(err);
+        //4.Print the Result
+        console.log(result);
+    })
+    //4.Process the Result
+    
+    //5.End Database Connection
+    connection.end();
+}
