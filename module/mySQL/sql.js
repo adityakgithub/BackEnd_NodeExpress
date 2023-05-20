686973aff854fe9476aeb0075d41b81704f3eadc
adityakgithub <adityakorade@gmail.com>
2023-05-20 17:26:18 +0530

    connection open added which is missed.

--- module/mySQL/sql.js
+++ module/mySQL/sql.js
@@ -1,16 +1,22 @@
 const mySql = require("mysql");
 
 function getProduct(){
-    //1.Create Connection 
-    const connection = mySql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "ecommerce_db"
+    //1.1Create Connection 
+        const connection = mySql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "root",
+            database: "ecommerce_db"
+        })
+    //1.2.Open Connection
+    connection.connect((err) => {
+        if (err) throw err;
     })
+
     //2.Create Database Query
     const statement =`select * from table_Name`
     //3.Execute the Query Created by Database
+    //asynchronous execution
     connection.query(statement, (err, result) => {
         if(err) throw err;
         console.log(err);
@@ -18,7 +24,7 @@ function getProduct(){
         console.log(result);
     })
     //4.Process the Result
-    
+
     //5.End Database Connection
     connection.end();
 }
